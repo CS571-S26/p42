8859a982fbb01f8d64ed0ed368244f63c04a9946
YoshiTheExplorer <colin.yamada@gmail.com>
2026-04-06 15:55:01 -0500

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center">
+      <p>Sub2Lease</p>
+    </footer>
+  );
+}
