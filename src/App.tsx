a01867613d204ec367122f44a9783760cf4a03e4
YoshiTheExplorer <colin.yamada@gmail.com>
2026-04-06 15:52:17 -0500

    created navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import NavigationBar from './components/NavigationBar';
 
 export default function App() {
   return (
     <BrowserRouter basename="/p42">
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,19 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export default function NavigationBar() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand as={Link} to="/">Sub2Lease</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-nav" />
+        <Navbar.Collapse id="main-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
