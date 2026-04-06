42f21c66a3f9ef8cd17f8714136c909193afe754
YoshiTheExplorer <colin.yamada@gmail.com>
2026-04-06 15:49:17 -0500

    created aboutpage

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+
+export default function AboutPage() {
+  return (
+    <Container className="my-5">
+      <h1>About the Product</h1>
+      <p>
+        Sub2Lease is a student subleasing platform designed to connect students looking for short-term housing with those who have available leases. Our mission is to make the subleasing process easier and more efficient for students, while also providing a safe and reliable platform for both parties.
+      </p>
+    </Container>
+  );
+}
