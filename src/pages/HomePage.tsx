d036078d83f7d77c4062757ed90ef1b26613f7df
YoshiTheExplorer <colin.yamada@gmail.com>
2026-04-06 15:36:43 -0500

    init homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+export default function HomePage() {
+  return (
+    <>
+      <Container className="my-5">
+        <h1>Welcome to the Home Page</h1>
+      </Container>
+    </>
+  );
+}
