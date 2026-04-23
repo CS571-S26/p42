eb3beb4c37f24c64b64cb0de09a3be5ad669a3a4
YoshiTheExplorer <colin.yamada@gmail.com>
2026-04-22 22:39:47 -0500

    improved footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="text-center">
-      <p>Sub2Lease</p>
+    <footer className="text-center py-4 bg-light mt-5">
+      <p className="mb-1">Sub2Lease</p>
+      <small>Student subleasing made simpler.</small>
     </footer>
   );
 }
